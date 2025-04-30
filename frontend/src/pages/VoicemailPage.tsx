f59bbcab45ffa25bc04f683acfd9e4ae9c4d5cbe
foxplorer <floridagator4443@proton.me>
2025-04-30 16:47:41 -0500

    updates

--- frontend/src/pages/VoicemailPage.tsx
+++ frontend/src/pages/VoicemailPage.tsx
@@ -927,7 +927,9 @@ const VoicemailPage: React.FC = () => {
             </IconButton>
           </Box>
         </Box>
-
+        <Typography variant="body1" color="text.secondary">
+          Thank you for testing the P2P Voicemail hackathon project. Only experiment with tiny micropayments and data that you're willing to lose.<br /><br />
+        </Typography>
         <VoicemailTabs
           contactsCount={contacts?.length || 0}
           onCreateVoicemail={() => (
